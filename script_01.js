0b788b04588c839567b36efc2bcc854d5f3fe7ff
Peter <schmidt.multimedia@gmail.com>
2021-11-19 10:19:29 +0100

    cotrol flow / vorbereitung

--- script_01.js
+++ script_01.js
@@ -1,2 +1,19 @@
 // Entscheidungsstrukturen | control flow
 
+// Deklaration + Assigment
+const ageJohn = 32; 
+const ageMark = 35; 
+
+// Deklaration
+let isJohnOlder,isJohnEqual;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+
